9d62b95c773d878172e6067f516b692c043bc92b
PareshRahool <113898696+PareshRahool@users.noreply.github.com>
2022-09-20 19:51:05 +0500

    Update streamlit_app.py
    Add fruitvice api and also import request library

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 
+
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -13,7 +14,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
+ 
 my_fruit_list=my_fruit_list.set_index('Fruit')
 
 
@@ -24,7 +25,16 @@ my_fruit_list=my_fruit_list.set_index('Fruit')
 # #display the table on the page
 # streamlit.dataframe(my_fruit_list)
 
+
+
 fruit_selected= streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruit_to_show= my_fruit_list.loc[fruit_selected]
 #display the table on the page
 streamlit.dataframe(fruit_to_show)
+
+#New Section to display fruitvice api response
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
+
