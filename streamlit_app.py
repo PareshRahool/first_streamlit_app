0b1447a2958d4c4d03bc155c8eb27b8e40f664e9
PareshRahool <113898696+PareshRahool@users.noreply.github.com>
2022-09-20 20:09:45 +0500

    Update streamlit_app.py
    Add fruit_choice variable that let user select fruit which we want to get information

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,10 @@ streamlit.dataframe(fruit_to_show)
 import requests
 
 streamlit.header("Fruitvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruit_choice=streamlit.text_input('What fruit would you like information about?','kiwi')
+streamlit.write('The user entered',fruit_choice)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json()) #just write the data to the screen
 
 #Take the json version of the response and normalize it
