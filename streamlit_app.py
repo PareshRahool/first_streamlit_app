5e6ba14e8a000e87adfa3d48ed354b4a556bb2bd
PareshRahool <113898696+PareshRahool@users.noreply.github.com>
2022-09-21 14:36:57 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,15 +38,32 @@ streamlit.dataframe(fruit_to_show)
 #import requests
 
 streamlit.header("Fruitvice Fruit Advice!")
-fruit_choice=streamlit.text_input('What fruit would you like information about?','kiwi')
-streamlit.write('The user entered',fruit_choice)
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-#streamlit.text(fruityvice_response.json()) #just write the data to the screen
 
-#Take the json version of the response and normalize it
-fruitvice_normalized=pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruitvice_normalized)
+try:
+    fruit_choice=streamlit.text_input('What fruit would you like information about?','kiwi')
+    if not fruit_choice:
+        streamlit.error("Please select a fruit to get information.")
+    else: 
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+        fruitvice_normalized=pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruitvice_normalized)
+
+except URLError as e:
+    streamlit.error()
+
+
+# fruit_choice=streamlit.text_input('What fruit would you like information about?','kiwi')
+# streamlit.write('The user entered',fruit_choice)
+# #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+# #streamlit.text(fruityvice_response.json()) #just write the data to the screen
+
+# #Take the json version of the response and normalize it
+# fruitvice_normalized=pandas.json_normalize(fruityvice_response.json())
+# streamlit.dataframe(fruitvice_normalized)
+
+
+
 
 #import snowflake.connector
 
